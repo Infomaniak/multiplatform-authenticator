cd1a5dadac12471522de25496ef592c9c10bb3e9
Louis CAD <louis.cognault@infomaniak.com>
2026-01-20 09:17:49 +0100

    chore: Revert "chore: Temporarily replace new AGP KMP library plugin"
    This reverts commit 0601bec7e21935a910c90abd30b0c1abd7b2366a.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,21 +2,16 @@ import com.android.build.api.dsl.androidLibrary
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
-    alias(core.plugins.android.library)
+    alias(core.plugins.android.kmp.library)
 }
 
 val androidCompileSdk: Int by rootProject.extra
-val appMinSdk: Int by rootProject.extra
-
-android {
-    namespace = "com.infomaniak.auth.multiplatform"
-    compileSdk = androidCompileSdk
-}
 
 kotlin {
-    @Suppress("DEPRECATION") //TODO[ik-auth]: Revert commit with this change once Android Studio Otter 3 is released.
-    androidTarget {
-        publishLibraryVariants("release")
+    @Suppress("UnstableApiUsage")
+    androidLibrary {
+        namespace = "com.infomaniak.auth.multiplatform"
+        compileSdk = androidCompileSdk
     }
     iosArm64()
 }
