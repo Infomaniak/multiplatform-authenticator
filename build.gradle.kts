a04416061170dc671a7898093b36121be252dc12
Vincent Te <vincent.te@infomaniak.com>
2026-01-27 11:09:06 +0100

    chore: Add export to XCFramework

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,9 @@ import com.android.build.api.dsl.androidLibrary
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(core.plugins.android.kmp.library)
+    alias(libs.plugins.skie)
+    id("infomaniak.kotlinMultiplatform")
+    id("infomaniak.publishPlugin")
 }
 
 val androidCompileSdk: Int by rootProject.extra
