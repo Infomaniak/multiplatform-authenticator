83c165440e3964e249f9f1b454a135fe74583a80
Vincent Te <vincent.te@infomaniak.com>
2026-02-12 09:40:45 +0100

    chore: Use catalog for tests libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,8 +57,8 @@ kotlin {
         }
         val androidDeviceTest by getting {
             dependencies {
-                implementation("androidx.test.ext:junit:1.1.5")
-                implementation("androidx.test.espresso:espresso-core:3.5.1")
+                implementation(libs.androidx.junit)
+                implementation(libs.androidx.espresso.core)
             }
         }
     }
