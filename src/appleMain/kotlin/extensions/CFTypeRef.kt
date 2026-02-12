d28fa5a0931ab6851e63024f72d7970892ab0b72
Louis CAD <louis.cognault@infomaniak.com>
2026-02-12 14:49:36 +0100

    feat: Introduce `use {}` extension for `CFTypeRef` subtypes

---
+++ src/appleMain/kotlin/extensions/CFTypeRef.kt
@@ -0,0 +1,37 @@
+/*
+ * Infomaniak Authenticator - Android
+ * Copyright (C) 2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+@file:OptIn(ExperimentalContracts::class)
+
+package com.infomaniak.auth.lib.extensions
+
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.CoreFoundation.CFRelease
+import platform.CoreFoundation.CFTypeRef
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+
+@OptIn(ExperimentalForeignApi::class)
+inline fun <T : CFTypeRef, R> T.use(block: (T) -> R): R {
+    contract { callsInPlace(block, InvocationKind.EXACTLY_ONCE) }
+    try {
+        return block(this)
+    } finally {
+        CFRelease(this)
+    }
+}
