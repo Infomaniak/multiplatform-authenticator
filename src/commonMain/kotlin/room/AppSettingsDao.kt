afb490d162e049bdb567b08ddeef2351d8a559a1
Vincent Te <vincent.te@infomaniak.com>
2026-02-18 10:00:33 +0100

    chore: Fix package

--- src/commonMain/kotlin/room/AppSettingsDao.kt
+++ src/commonMain/kotlin/room/AppSettingsDao.kt
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.infomaniak.auth.room
+package com.infomaniak.auth.lib.room
 
 import androidx.room.Dao
 import androidx.room.Insert
