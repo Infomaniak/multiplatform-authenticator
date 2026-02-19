7424a6773dda09f2d9deebac76f659af3af507c0
Vincent Te <vincent.te@infomaniak.com>
2026-02-19 08:32:04 +0100

    chore: Rename package name

--- src/androidMain/kotlin/db/AppSettingsDatabase.android.kt
+++ src/androidMain/kotlin/db/AppSettingsDatabase.android.kt
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.infomaniak.auth.lib
+package com.infomaniak.auth.lib.db
 
 import android.content.Context
 import androidx.room.Room
