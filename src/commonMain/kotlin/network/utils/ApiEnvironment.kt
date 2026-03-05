8f904d067feb91f878fe050195d345094faa436a
Vincent Te <vincent.te@infomaniak.com>
2026-03-05 15:04:34 +0100

    chore: Remove unused parameter

--- src/commonMain/kotlin/network/utils/ApiEnvironment.kt
+++ src/commonMain/kotlin/network/utils/ApiEnvironment.kt
@@ -22,6 +22,6 @@ sealed class ApiEnvironment(val baseUrl: String) {
     data object Preprod :
         ApiEnvironment("https://authenticator.preprod.dev.infomaniak.ch") //TODO Change this to the final baseUrl
 
-    data object Prod : ApiEnvironment("https://www.authenticator.com")  //TODO Change this to the final baseUrl
+    data object Prod : ApiEnvironment("")
     data class Custom(private val url: String) : ApiEnvironment(url)
 }
