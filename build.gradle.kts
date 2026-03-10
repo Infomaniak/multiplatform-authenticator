96dafe7eecb6e262c1c7c719d8c946faf3fc8ca2
Vincent Te <vincent.te@infomaniak.com>
2026-03-10 12:48:57 +0100

    chore: Change json dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,7 +53,7 @@ kotlin {
         commonMain {
             dependencies {
                 implementation(core.kotlinx.coroutines.core)
-                implementation(core.kotlinx.serialization.core)
+                implementation(core.kotlinx.serialization.json)
                 implementation(core.kotlinx.serialization.cbor)
                 implementation(core.ktor.client.core)
                 implementation(core.ktor.client.content.negociation)
