e86efacf2ae811ef140449c7386e180d59104c74
Jamy Bailly <jamy.bailly@infomaniak.com>
2026-03-20 11:33:23 +0100

    feat: Add Url actions to manager

---
+++ src/commonMain/kotlin/network/models/UrlConstant.kt
@@ -0,0 +1,30 @@
+/*
+ * Infomaniak Authenticator - Android
+ * Copyright (C) 2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.auth.lib.network.models
+
+object UrlConstant {
+    private fun managerUrl(host: String) = "https://manager.$host/v3"
+    fun managerUrl(host: String, path: String) = "${managerUrl(host)}/$path"
+
+    private fun autologUrl(host: String) = "${managerUrl(host)}/$AUTOLOG_URL"
+    fun autologUrl(host: String, url: String) = "${autologUrl(host)}/?url=$url"
+
+    const val AUTOLOG_URL = "mobile_login"
+    const val ACTIVITY_MANAGER_URL = "ng/profile/user/connection-history/activity"
+    const val SETTINGS_MANAGER_URL = "ng/profile/user/security-and-recovery-parameters/dashboard"
+}
