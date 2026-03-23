20eeb934f5280dc884a9a6db43268fc1ca2dd80b
Louis CAD <louis.cognault@infomaniak.com>
2026-03-23 16:33:54 +0100

    fix: Fix package directive of WebAuthnTest.kt

--- src/androidDeviceTest/kotlin/WebAuthnTest.kt
+++ src/androidDeviceTest/kotlin/WebAuthnTest.kt
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.infomaniak.auth
+package com.infomaniak.auth.lib
 
 import com.infomaniak.auth.lib.CryptoObjectsBuilder
 import com.infomaniak.auth.lib.internal.KeyPairManagerImpl
