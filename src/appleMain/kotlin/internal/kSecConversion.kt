c1a2472e66ee617b3d12f13f5ad33ce4000b0340
Louis CAD <louis.cognault@infomaniak.com>
2026-03-24 14:10:29 +0100

    fix: Fix KeyAccessibility mapping

--- src/appleMain/kotlin/internal/kSecConversion.kt
+++ src/appleMain/kotlin/internal/kSecConversion.kt
@@ -38,8 +38,8 @@ import platform.Security.kSecAttrAccessibleWhenUnlockedThisDeviceOnly
 internal fun KeyAccessibility.toKSecAttrAccessible(): CFStringRef? = when (this) {
     KeyAccessibility.WhenUnlocked.ThisDeviceOnly -> kSecAttrAccessibleWhenUnlockedThisDeviceOnly
     KeyAccessibility.WhenUnlocked -> kSecAttrAccessibleWhenUnlocked
-    KeyAccessibility.AfterFirstUnlock.ThisDeviceOnly -> kSecAttrAccessibleAfterFirstUnlock
-    KeyAccessibility.AfterFirstUnlock -> kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly
+    KeyAccessibility.AfterFirstUnlock.ThisDeviceOnly -> kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly
+    KeyAccessibility.AfterFirstUnlock -> kSecAttrAccessibleAfterFirstUnlock
     KeyAccessibility.WhenPasscodeSet.ThisDeviceOnly -> kSecAttrAccessibleWhenPasscodeSetThisDeviceOnly
     KeyAccessibility.Always -> kSecAttrAccessibleAlways
     KeyAccessibility.Always.ThisDeviceOnly -> kSecAttrAccessibleAlwaysThisDeviceOnly
