f83f24dc0d1051afe30ce653c8b690513bbb1dd5
Vincent Te <vincent.te@infomaniak.com>
2026-06-02 09:28:26 +0200

    chore: Persist the user when we call addAccounts

--- src/commonMain/kotlin/internal/AuthenticatorFacadeImpl.kt
+++ src/commonMain/kotlin/internal/AuthenticatorFacadeImpl.kt
@@ -129,6 +129,7 @@ internal class AuthenticatorFacadeImpl(
         .shareIn(coroutineScope, SharingStarted.Eagerly, replay = 1)
 
     override suspend fun addAccounts(connectedAccounts: List<SharedUserProfile>) {
+        connectedAccounts.forEach { authenticatorBridge.persistUserProfile(it) }
         val entities = connectedAccounts.map {
             it.toAccountEntity(Status.PasskeyRegistrationPending)
         }
