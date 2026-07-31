52b239cedb156a9b6d2aebf476b02b0fad7c97c4
Jamy Bailly <jamy.bailly@infomaniak.com>
2026-07-31 15:17:16 +0200

    feat: Make repo as project with split from app project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,43 @@
+/*
+ * Infomaniak Authenticator - Multiplatform
+ * Copyright (C) 2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import co.touchlab.skie.configuration.DefaultArgumentInterop
 import com.android.build.api.dsl.androidLibrary
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
 
+buildscript {
+    extra.apply {
+        set("androidCompileSdk", 36)
+        set("androidMinSdk", 27)
+        set("javaVersion", JavaVersion.VERSION_17)
+    }
+}
+
 plugins {
-    alias(libs.plugins.kotlin.multiplatform)
-    alias(core.plugins.android.kmp.library)
-    alias(core.plugins.kotlin.serialization)
-    alias(libs.plugins.skie)
-    alias(libs.plugins.androidx.room)
-    alias(libs.plugins.ksp)
-    kotlin("plugin.parcelize")
+    alias(kmpAuthenticator.plugins.kotlin.multiplatform)
+    alias(kmpAuthenticator.plugins.android.kmp.library)
+    alias(kmpAuthenticator.plugins.kotlin.serialization)
+    alias(kmpAuthenticator.plugins.skie)
+    alias(kmpAuthenticator.plugins.androidx.room)
+    alias(kmpAuthenticator.plugins.ksp)
+    kotlin("plugin.parcelize") version kmpAuthenticator.versions.kotlin
+    id("infomaniak.publishPlugin")
 }
 
 val androidCompileSdk: Int by rootProject.extra
@@ -50,43 +77,43 @@ kotlin {
 
     sourceSets {
         commonMain.dependencies {
-            implementation(libs.androidx.room.runtime)
-            implementation(libs.androidx.sqlite.bundled)
+            implementation(kmpAuthenticator.androidx.room.runtime)
+            implementation(kmpAuthenticator.androidx.sqlite.bundled)
         }
     }
 
     sourceSets {
         commonMain {
             dependencies {
-                implementation(core.kotlinx.coroutines.core)
-                implementation(core.kotlinx.serialization.json)
-                implementation(core.kotlinx.serialization.cbor)
-                implementation(core.ktor.client.core)
-                implementation(core.ktor.client.auth)
-                implementation(core.ktor.client.content.negociation)
-                implementation(core.ktor.client.json)
-                implementation(core.ktor.client.encoding)
-                implementation(core.okio)
-                implementation(libs.osmerion.kotlin.base32)
+                implementation(kmpAuthenticator.kotlinx.coroutines.core)
+                implementation(kmpAuthenticator.kotlinx.serialization.json)
+                implementation(kmpAuthenticator.kotlinx.serialization.cbor)
+                implementation(kmpAuthenticator.ktor.client.core)
+                implementation(kmpAuthenticator.ktor.client.auth)
+                implementation(kmpAuthenticator.ktor.client.content.negociation)
+                implementation(kmpAuthenticator.ktor.client.json)
+                implementation(kmpAuthenticator.ktor.client.encoding)
+                implementation(kmpAuthenticator.okio)
+                implementation(kmpAuthenticator.osmerion.kotlin.base32)
             }
         }
         commonTest {
             dependencies {
                 implementation(kotlin("test"))
-                implementation(core.kotlinx.coroutines.test)
-                implementation(core.ktor.client.mock)
+                implementation(kmpAuthenticator.kotlinx.coroutines.test)
+                implementation(kmpAuthenticator.ktor.client.mock)
             }
         }
         androidMain {
             dependencies {
-                implementation(core.ktor.client.okhttp)
-                implementation(core.splitties.appctx)
-                implementation(core.splitties.bitflags)
+                implementation(kmpAuthenticator.ktor.client.okhttp)
+                implementation(kmpAuthenticator.splitties.appctx)
+                implementation(kmpAuthenticator.splitties.bitflags)
             }
         }
         appleMain {
             dependencies {
-                implementation(core.ktor.client.darwin)
+                implementation(kmpAuthenticator.ktor.client.darwin)
             }
         }
 
@@ -98,8 +125,8 @@ kotlin {
 
         val androidDeviceTest by getting {
             dependencies {
-                implementation(core.androidx.junit)
-                implementation(core.androidx.espresso.core)
+                implementation(kmpAuthenticator.androidx.junit)
+                implementation(kmpAuthenticator.androidx.espresso.core)
             }
         }
     }
@@ -127,10 +154,10 @@ room {
 }
 
 dependencies {
-    add("kspAndroid", libs.androidx.room.compiler)
-    add("kspIosSimulatorArm64", libs.androidx.room.compiler)
-    add("kspIosArm64", libs.androidx.room.compiler)
-    add("kspMacosArm64", libs.androidx.room.compiler)
+    add("kspAndroid", kmpAuthenticator.androidx.room.compiler)
+    add("kspIosSimulatorArm64", kmpAuthenticator.androidx.room.compiler)
+    add("kspIosArm64", kmpAuthenticator.androidx.room.compiler)
+    add("kspMacosArm64", kmpAuthenticator.androidx.room.compiler)
 }
 
 listOf("IosArm64", "IosSimulatorArm64", "MacosArm64").forEach { target ->
