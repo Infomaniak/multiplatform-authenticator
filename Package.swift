0712c483467490ca0fdc663074d0e98974946805
Jamy Bailly <jamy.bailly@infomaniak.com>
2026-08-05 15:29:18 +0200

    feat: Can now release snapshot

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.10
+import PackageDescription
+
+let package = Package(
+    name: "MultiplatformAuthenticator",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v12),
+    ],
+    products: [
+        .library(name: "CoreAuthenticator", targets: ["CoreAuthenticator"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "CoreAuthenticator",
+            // Placeholder URL/checksum: automatically overwritten by the first run of
+            // the "Release KMP library" workflow (see .github/workflows/release-library.yml).
+            url: "https://github.com/Infomaniak/multiplatform-authenticator/releases/download/0.0.0/CoreAuthenticator.xcframework.zip",
+            checksum: "0000000000000000000000000000000000000000000000000000000000000000000000"
+        ),
+    ]
+)
